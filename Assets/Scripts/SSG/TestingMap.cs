1260dcbbd078136ddc674e552d15fecc526f59b0
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-03-12 19:37:52 +0900

    Test-TestingMap

--- Assets/Scripts/SSG/TestingMap.cs
+++ Assets/Scripts/SSG/TestingMap.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -21,44 +22,58 @@ public class TestingMap : Singleton<TestingMap>
          {0,0,0,0,0,0,0,0,0,0,0,0},
          {0,0,0,0,0,0,0,0,0,0,0,0}
         };
-    void MapData()
-    {
-        var Map = new List<int>()
-        {
-            
-        };
+    //가로
+    //세로
+    //void MapData()
+    //{
+    //    var Map = new List<int[]>()
+    //    {
+    //        {0,0,0,0, }
+    //    };       
         
-    }
-    
+    //}
+    void SpawnMap()
+    {
+        foreach(var map in MapManager.Instance.stage1)
+        {
+            if(map.itemIndex==0)
+            {
+                Instantiate(floor);
+                floor.transform.position = new Vector3(map.x, map.y);
+                Debug.Log($"생성했다 x:{map.x}y:{map.y}");
+            }
+        }
+    }  
     
 
     private void Start()
     {
-        StartMap();
-        //Debug.Log("디버그 어디감?");
+        //StartMap();
+        SpawnMap();
+        
     }    
-    private void StartMap()
-    {        
-        Vector3 vector;
-        int ypos = map.GetLength(1);
-        int xpos = map.GetLength(0) ;
-        int startpos = xpos - 1;
-        for(int i= 0;i< ypos; i++)//y
-        {            
-            for (int j=0;j< xpos; j++)//xypos
-            {
-                //생성 데이터로  맵 관리
-                if (map[startpos - j, i] == 0)
-                {
-                    //오브젝트 데이터를 어떻게 받아서 맵을 생성시킬까>
+    //private void StartMap()
+    //{        
+    //    Vector3 vector;
+    //    int ypos = map.GetLength(1);
+    //    int xpos = map.GetLength(0) ;
+    //    int startpos = xpos - 1;
+    //    for(int i= 0;i< ypos; i++)//y
+    //    {            
+    //        for (int j=0;j< xpos; j++)//xypos
+    //        {
+    //            //생성 데이터로  맵 관리
+    //            //if (map[startpos - j, i] == 0)
+    //            //{
+    //                //오브젝트 데이터를 어떻게 받아서 맵을 생성시킬까>
                     
-                    vector = new Vector3(i, j);
-                    Instantiate(floor);//오브젝트데이터를 받아서 생성시키게 하기
-                    floor.transform.position = vector;
-                    //Debug.Log($"{floor.transform.position}");
-                }
+    //                vector = new Vector3(i, j);
+    //                Instantiate(floor);//오브젝트데이터를 받아서 생성시키게 하기
+    //                floor.transform.position = vector;
+    //                //Debug.Log($"{floor.transform.position}");
+    //            //}
                 
-            }          
-        }
-    }    
+    //        }          
+    //    }
+    //}    
 }
