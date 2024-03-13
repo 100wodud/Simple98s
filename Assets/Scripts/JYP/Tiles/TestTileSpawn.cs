658f7812c5331637a3daa714270403919779a10b
Jaeyoung Paik <100wodud@gmail.com>
2024-03-13 16:26:01 +0900

    Fix - Conflict 해결

--- Assets/Scripts/JYP/Tiles/TestTileSpawn.cs
+++ Assets/Scripts/JYP/Tiles/TestTileSpawn.cs
@@ -10,29 +10,15 @@ public class TestTileSpawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UnityGoogleSheet.LoadAllData();
-        tiles = Simple98.Tiles.TilesList;
-        /*
-        foreach (var data in Simple98.Tiles.TilesList)
-        {
-            newObject(data.localeID);
-
-        }
-        // 구글 시트 맵 데이터 둘러보기
-        foreach (var data in Simple98.Maps.MapsList)
-        {
-            newObject(data.);
-
-        }
-        */
         DataManager.Instance.Initialize();
 
         const string path = "Prefabs/";
-
+        /*
         foreach (var item in DataManager.instance.stage1)
         {
             Instantiate(Resources.Load($"{path + DataManager.instance.tiles[item.Tile].Type + "/" + DataManager.instance.tiles[item.Tile].localeID}"), new Vector3(item.Row, -item.Column, 0), Quaternion.identity);
         }
+        */
 
     }
 }
