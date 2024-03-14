848e920fc2ec9964c716596602107b1c44e84141
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-03-14 16:05:48 +0900

    fix.DataManager.cs

--- Assets/Scripts/JYP/Manager/DataManager.cs
+++ Assets/Scripts/JYP/Manager/DataManager.cs
@@ -5,7 +5,7 @@ using UGS;
 using UnityEngine;
 
 public class DataManager : Singleton<DataManager>
-{
+{    
     //모든 데이터 매니저의 데이터를 가지고 있는 매니저
     //데이터는 데이터 매니저를 통해서 불러오기
     //Sheet에 있는 Data 저장
@@ -15,12 +15,18 @@ public class DataManager : Singleton<DataManager>
     //맵 데이터매니저
     
     public List<Tiles> tiles = new List<Tiles>();
-    public Dictionary<int, Maps> Map = new Dictionary<int, Maps>();
-    public void Initialize()//초기화
-    {
+    public Dictionary<int, Maps> Map = new Dictionary<int, Maps>();    
+    public MapDatas MapData = new MapDatas();
+    //맵 데이터스 함수데이터를 받게하기
+    public void Initialize()//초기화   
+    {//스테이지 데이터를 맵 데이터 안에다가 넣기        
+        MapData.Initialize();
         UnityGoogleSheet.Load<Maps>();
-        UnityGoogleSheet.Load<Tiles>();
+        UnityGoogleSheet.Load<Tiles>();        
         Map = Maps.MapsMap;
-        tiles = Tiles.TilesList;
+        tiles = Tiles.TilesList;        
     }   
+    
+
+    
 }
