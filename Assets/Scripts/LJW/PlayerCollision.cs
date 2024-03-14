83530c7f4c8d66c0e74ce911c2991caa525362b2
kimjunseo <dpdl33000@naver.com>
2024-03-14 19:04:53 +0900

    Feat - hpui 추가

--- Assets/Scripts/LJW/PlayerCollision.cs
+++ Assets/Scripts/LJW/PlayerCollision.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
 
 public class PlayerCollision : MonoBehaviour
 {
+    private HpControll hpControll;
+
+    private void Start()
+    {
+        hpControll = FindObjectOfType<HpControll>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.transform.tag == "Obstacle")
+        if (collision.transform.tag == "Obstacle")
         {
             PlayerHealth.health--;
+            hpControll.HpMinus();
             Debug.Log("Player health: " + PlayerHealth.health);
             if (PlayerHealth.health < 0)
             {
