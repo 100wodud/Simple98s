25f0fb109a9b3bec8b181d226d5d6f420a389e67
kimjunseo <dpdl33000@naver.com>
2024-03-14 19:28:53 +0900

    fix - hp수정

--- Assets/Scripts/LJW/PlayerCollision.cs
+++ Assets/Scripts/LJW/PlayerCollision.cs
@@ -5,17 +5,23 @@ using System.Collections;
 
 public class PlayerCollision : MonoBehaviour
 {
+    private HpControll hpControll;
+
+    private void Start()
+    {
+        hpControll = FindObjectOfType<HpControll>();
+    }
     private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
     {
-        if(collision.transform.tag == "Obstacle")
+        if (collision.transform.tag == "Obstacle")
         {
             PlayerHealth.health--;
+            hpControll.HpMinus();
             Debug.Log("Player health: " + PlayerHealth.health);
-            if (PlayerHealth.health < 0)
+            if (PlayerHealth.health <= 0)
             {
                 Debug.Log("Gameover");
             }
         }
     }
-
 }
