31789c73593053a06ffd2ce6070041fc60d7db3e
Jaeyoung Paik <100wodud@gmail.com>
2024-03-15 15:35:41 +0900

    Fix - Wall, Player Prefabs 수정

--- Assets/Scripts/SSG/GameManager.cs
+++ Assets/Scripts/SSG/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : Singleton<GameManager>
     private void Awake()
     {
         DataManager.Instance.Initialize();
-        MapManager.Instance.MakeStage(3);
+        MapManager.Instance.MakeStage(2);
     }
     
 }
