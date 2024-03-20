4bab649ab3569c07dde68eb77e9639cebb1fc5de
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-03-20 19:36:13 +0900

    Fix-GameManager

--- Assets/Scripts/SSG/GameManager.cs
+++ Assets/Scripts/SSG/GameManager.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
-{    
+{      
+    
     protected override void Awake()
-    {
+    {       
         DataManager.Instance.Initialize();
-        MapManager.Instance.MakeStage(2);
+        //MapManager.Instance.MakeStage(StageManager.Instance.stageindex);
     }
     
 }
