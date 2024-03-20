172f081e4962d2a2626ead1929bdc00acffd4bf9
kimjunseo <dpdl33000@naver.com>
2024-03-20 11:53:53 +0900

    fix - 스테이지 재선택 수정

--- Assets/Scripts/KJS/Trash/PlusStar.cs
+++ Assets/Scripts/KJS/Trash/PlusStar.cs
@@ -6,55 +6,78 @@ using UnityEngine.UI;
 public class PlusStar : MonoBehaviour
 {
 
-    private StageSelectionUI stageSelectionUI;
+    private Popup_StageSelect popup_StageSelect;
 
-    private StageStarData stage1StarData; // 스테이지 1의 별 정보를 저장할 변수
+    private StageStarData stageStarData; // 스테이지 별 정보를 저장할 변수
 
-    private int stageLevel = 0;
+    private int _stageLevel = 0;
 
     private void Start()
     {
-        stage1StarData = StarManager.Instance.stageStarDataArray[stageLevel]; // 스테이지 1의 별 정보 가져오기
-        stageSelectionUI = GetComponent<StageSelectionUI>();
+        popup_StageSelect = FindObjectOfType<Popup_StageSelect>();
+    }
+    public void OnClickStage()
+    {
+        if (popup_StageSelect != null)
+        {
+            popup_StageSelect.Destroy();
+            popup_StageSelect = UIManager.Instance.ShowPopup<Popup_StageSelect>();
+            popup_StageSelect.Initialize();
+            popup_StageSelect.UpdateStar(_stageLevel);
+        }
+        else
+        {
+            popup_StageSelect = UIManager.Instance.ShowPopup<Popup_StageSelect>();
+            popup_StageSelect.Initialize();
+            popup_StageSelect.UpdateStar(_stageLevel);
+        }
+
+    }
+    public void SelectStageOne()
+    {
+        _stageLevel = 0;
+        //GetStageStar();
+        Debug.Log((_stageLevel + 1) + "level");
+        //Debug.Log(stageStarData.starsEarned + "star");
     }
 
-    public void setZeroStar()
+    public void SelectStageTwo()
     {
-        StarManager.Instance.SetStarsForStage(stageLevel, 0);
-        stageSelectionUI.UpdateStar(stageLevel);
+        _stageLevel = 1;
+        //GetStageStar();
+        Debug.Log((_stageLevel + 1) + "level");
+        //Debug.Log(stageStarData.starsEarned + "star");
+    }
+    private void GetStageStar()
+    {
+        stageStarData = popup_StageSelect.stageStarDataArray [_stageLevel]; // 스테이지 별 정보 가져오기
     }
 
-    public void setOneStar()
+    public void SpawnStageInfo()
     {
-        StarManager.Instance.SetStarsForStage(stageLevel, 1);
-        stageSelectionUI.UpdateStar(stageLevel);
+
     }
-    public void setTwoStar()
+    public void SetZeroStar()
     {
-        StarManager.Instance.SetStarsForStage(stageLevel, 2);
-        stageSelectionUI.UpdateStar(stageLevel);
-    }
-    public void setThreeStar()
-    {
-        StarManager.Instance.SetStarsForStage(stageLevel, 3);
-        stageSelectionUI.UpdateStar(stageLevel);
-    }
-    public void AddStar()
-    {
-        stage1StarData.starsEarned++;
-        Debug.Log("+1");
-        StarManager.Instance.SetStarsForStage(0, stage1StarData.starsEarned);
-        stageSelectionUI.UpdateStar(stageLevel);
+        popup_StageSelect.SetStarsForStage(_stageLevel, 0);
+        popup_StageSelect.UpdateStar(_stageLevel);
     }
 
-    public void MinuStar()
+    public void SetOneStar()
     {
-        stage1StarData.starsEarned--;
-        Debug.Log("-1");
-        StarManager.Instance.SetStarsForStage(0, stage1StarData.starsEarned);
-        stageSelectionUI.UpdateStar(stageLevel);
+        popup_StageSelect.SetStarsForStage(_stageLevel, 1);
+        popup_StageSelect.UpdateStar(_stageLevel);
     }
-
+    public void SetTwoStar()
+    {
+        popup_StageSelect.SetStarsForStage(_stageLevel, 2);
+        popup_StageSelect.UpdateStar(_stageLevel);
+    }
+    public void SetThreeStar()
+    {
+        popup_StageSelect.SetStarsForStage(_stageLevel, 3);
+        popup_StageSelect.UpdateStar(_stageLevel);
+    }
     
 }
 
--- Assets/Scripts/KJS/Trash/Popup_StageSelect.cs
+++ Assets/Scripts/KJS/Trash/Popup_StageSelect.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class StarManager : MonoBehaviour
+public class Popup_StageSelect : UIPopup
 {
+    [SerializeField]private Image[] _starImages; // 스테이지 선택 화면 UI에서 별을 표시할 이미지 배열
     public StageStarData[] stageStarDataArray; // 각 스테이지의 별 정보를 담을 배열
 
+    public void Initialize() //초기화 메서드
+    {
+        Refresh();
+    }
+    private void Refresh()
+    {
+
+    }
 
     // 스테이지에서 얻은 별 개수를 설정하는 함수
     public void SetStarsForStage(int stageIndex, int stars)
@@ -28,5 +38,21 @@ public class StarManager : MonoBehaviour
             PlayerPrefs.SetInt("StarsForStage_" + i, stageStarDataArray[i].starsEarned);
         }
         PlayerPrefs.Save(); // 변경 사항 저장
+    }
+
+    public void UpdateStar(int s)
+    {
+        UpdateStarsUI(s); // 스테이지 1에 대한 별 정보를 가져와서 UI에 표시
+    }
+
+    // 스테이지 선택 화면 UI에 별을 표시하는 함수
+    private void UpdateStarsUI(int stageIndex)
+    {
+        int stars = GetStarsForStage(stageIndex); // 스테이지 별 정보 가져오기
+
+        for (int i = 0; i < _starImages.Length; i++)
+        {
+            _starImages[i].gameObject.SetActive(i < stars); // 별 개수에 따라 이미지 활성화/비활성화
+        }
     }
 }
