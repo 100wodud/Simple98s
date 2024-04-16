af6a8b26f5da259b227373ca5b985fd3b2d4b6c9
kimjunseo <dpdl33000@naver.com>
2024-04-16 13:50:50 +0900

    fix - bgm 변경

--- Assets/Scripts/KJS/UI/DontDestroy.cs
+++ Assets/Scripts/KJS/UI/DontDestroy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroy : MonoBehaviour
 {
@@ -16,5 +17,37 @@ public class DontDestroy : MonoBehaviour
 
         dontDestroyObjects.Add(gameObject.name);
         DontDestroyOnLoad(gameObject);
+    }
+
+    [SerializeField] private AudioSource bgm1;
+    [SerializeField] private AudioSource bgm2;
+    public void Start()
+    {
+        bgm1.Play();
+        bgm2.Play();
+    }
+
+    public void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "StageScene" || SceneManager.GetActiveScene().name == "CustomStageScene")
+        {
+            bgm1.Stop();
+            bgm1.gameObject.SetActive(false);
+            if (bgm2.gameObject.activeSelf == false) 
+            {
+                bgm2.gameObject.SetActive(true);
+                bgm2.Play();
+            }
+        }
+        else
+        {
+            bgm2.Stop();
+            bgm2.gameObject.SetActive(false);
+            if (bgm1.gameObject.activeSelf == false)
+            {
+                bgm1.gameObject.SetActive(true);
+                bgm1.Play();
+            }
+        }
     }
 }
