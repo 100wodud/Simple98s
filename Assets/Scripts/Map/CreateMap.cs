444db1ad63b19fd8095c06b652332f3345f797d7
Jaeyoung Paik <100wodud@gmail.com>
2024-05-01 12:04:33 +0900

    Delete - 필요없는 폴더 정리

--- Assets/Scripts/Map/CreateMap.cs
+++ Assets/Scripts/Map/CreateMap.cs
@@ -7,11 +7,6 @@ public class CreateMap : MonoBehaviour
 {    
     void Start()
     {
-        //Debug.Log(StageSelecter.selectStageIndex);
-        //JsonDataManager.Instance.IndexLoad();
         MapManager.Instance.MakeStage(StageSelecter.selectStageIndex);
-        //코인 데이터 사용
-        //스테이지 클리어 사용
-        //스테이지 별 사용
     }
 }
